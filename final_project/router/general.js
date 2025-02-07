a8906ff86d2c712375f9e517504811772fecf0b3
Diego <diegomercado2002@gmail.com>
2025-02-07 23:53:44 +0100

    feat: get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,7 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
-  
+
   if (book) {
       res.status(200).json(book);
   } else {
@@ -29,6 +29,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+  booksByAuthor = [];
+  const author = req.params.author;
+  for (let i in books) {
+      if (books[i].author === author) {
+          booksByAuthor.push(books[i]);
+      }
+  }
+  
+  if (booksByAuthor.length > 0) {
+      res.status(200).json(booksByAuthor);
+  } else {
+      res.status(404).json({ message: 'Author not found' });
+  }
+
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
