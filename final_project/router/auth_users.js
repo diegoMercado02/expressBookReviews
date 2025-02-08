570b00edb520b26f5586c69afa24afe2b982869a
Diego <diegomercado2002@gmail.com>
2025-02-08 01:38:36 +0100

    feat: added review and delete review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -37,8 +37,62 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const token = req.headers.authorization.split(' ')[1];
+  const decoded = jwt.verify(token, "fingerprint_customer");
+  const username = decoded.username;
+
+  if (!books[isbn]) {
+
+    return res.status(404).json({ message: "Book not found" });
+
+  } 
+  else if (!review) {
+    return res.status(400).json({ message: "Review is required" });
+  } 
+  else {
+    if (!Array.isArray(books[isbn].reviews)) {
+      books[isbn].reviews = [];
+    }
+
+    if (books[isbn].reviews.length === 0 || books[isbn].reviews.length === undefined) {
+      books[isbn].reviews.push({ username, review });
+      return res.status(200).json({ message: "Review added successfully", reviews: books[isbn].reviews[0] });
+
+    } else if (books[isbn].reviews.length > 0) {
+      const userReviewIndex = books[isbn].reviews.findIndex(r => r.username === username);
+
+      if (userReviewIndex >= 0) {
+        books[isbn].reviews[userReviewIndex].review = review;
+        return res.status(200).json({ message: "Review updated successfully", review: books[isbn].reviews });
+      } else {
+        books[isbn].reviews.push({ username, review });
+        return res.status(200).json({ message: "Review added successfully", review: books[isbn].reviews });
+      }
+    }
+  }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const token = req.headers.authorization.split(' ')[1];
+  const decoded = jwt.verify(token, "fingerprint_customer");
+  const username = decoded.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  } else {
+    const userReviewIndex = books[isbn].reviews.findIndex(r => r.username === username);
+
+    if (userReviewIndex >= 0) {
+      books[isbn].reviews.splice(userReviewIndex, 1);
+      return res.status(200).json({ message: "Review deleted successfully", review: books[isbn].reviews  });
+    } else {
+      return res.status(404).json({ message: "Review not found", review: books[isbn].reviews  });
+    }
+  }
 });
 
 module.exports.authenticated = regd_users;
